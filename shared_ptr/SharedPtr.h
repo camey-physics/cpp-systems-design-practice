26f2ca866fc3894cbab5e526a13f32d59eb0058e
Chris Amey <callen.amey@gmail.com>
2025-04-23 14:59:18 -0400

    Implemented SharedPtr header to satisfy rule of three.

--- shared_ptr/SharedPtr.h
+++ shared_ptr/SharedPtr.h
@@ -2,7 +2,16 @@
 #define SHARED_PTR_H
 
 class SharedPtr {
+    public:
+        SharedPtr() {};
+        SharedPtr(int size);
+        SharedPtr(const SharedPtr& other);
+        ~SharedPtr();
+        SharedPtr& operator=(const SharedPtr& other);
 
+    private:
+        int* ptr_ = nullptr;
+        int* ref_count_ = nullptr;
 };
 
 #endif
