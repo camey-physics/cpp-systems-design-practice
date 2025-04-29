df768a23e5b2a9addac06039682702bfbd52e5da
Chris Amey <callen.amey@gmail.com>
2025-04-29 17:40:08 -0400

    Implemented templated SharedPtr class and added two tests, one for floats and one for a self-defined square class.

--- include/SharedPtr.h
+++ include/SharedPtr.h
@@ -1,24 +1,114 @@
 #ifndef SHARED_PTR_H
 #define SHARED_PTR_H
 
+#include <cassert>
+
+template <typename T>
 class SharedPtr {
     public:
-        SharedPtr(int value = 0);
+        SharedPtr(T value = T());
         SharedPtr(const SharedPtr& other);
         ~SharedPtr();
         SharedPtr& operator=(const SharedPtr& other);
-        int& operator*();
-        const int& operator*() const;
+        T& operator*();
+        const T& operator*() const;
 
         int useCount() const;
-        const int* get() const;
+        const T* get() const;
 
     private:
-        int* ptr_ = nullptr;
+        T* ptr_ = nullptr;
         int* ref_count_ = nullptr;
 
         void incrementRefCount();
         void decrementAndCleanup();
 };
+
+template <typename T>
+void SharedPtr<T>::incrementRefCount() {
+    if (ref_count_ == nullptr) {
+        ref_count_ = new int;
+        *ref_count_ = 1;
+    }
+    else {
+        *ref_count_ += 1;
+    }
+};
+
+template <typename T>
+void SharedPtr<T>::decrementAndCleanup() {
+    if (ref_count_ == nullptr) {
+        assert((ptr_ == nullptr) == (ref_count_ == nullptr));
+        return;
+    }
+    else if (*ref_count_ > 1) {
+        *ref_count_ -= 1;
+    }
+    else {
+        delete ptr_;
+        delete ref_count_;
+    }
+}
+
+template <typename T>
+SharedPtr<T>::~SharedPtr() {
+    decrementAndCleanup();
+}
+
+template <typename T>
+SharedPtr<T>::SharedPtr(T value) {
+    ptr_ = new T(value);
+    ref_count_ = new int;
+    *ref_count_ = 1;
+}
+
+// Copy constructor for SharedPtr b = a; (a = other, b is being constructed)
+template <typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr& other) {
+    ptr_ = other.ptr_;
+    ref_count_ = other.ref_count_;
+    incrementRefCount();
+}
+
+// Copy assignment operator (overload  =)
+template <typename T>
+SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& other){
+    if (this == &other) {
+        return *this;
+    }
+    decrementAndCleanup();
+
+    ptr_ = other.ptr_;
+    ref_count_ = other.ref_count_;
+    incrementRefCount();
+    return *this;
+}
+
+template <typename T>
+int SharedPtr<T>::useCount() const {
+    if (ref_count_ == nullptr) {
+        return 0;
+    }
+    else {
+        return *ref_count_;
+    }
+}
+
+template <typename T>
+const T* SharedPtr<T>::get() const {
+    return ptr_;
+}
+
+template <typename T>
+T& SharedPtr<T>::operator*() {
+    assert(ptr_ != nullptr);
+    return *ptr_;
+}
+
+template <typename T>
+const T& SharedPtr<T>::operator*() const {
+    assert(ptr_ != nullptr);
+    return *ptr_; 
+}
 
 #endif
--- src/SharedPtr.cpp
+++ src/SharedPtr.cpp
@@ -1,78 +1,89 @@
-#include "SharedPtr.h"
-#include <cassert>
+// #include "SharedPtr.h"
+// #include <cassert>
 
-void SharedPtr::incrementRefCount() {
-    if (ref_count_ == nullptr) {
-        ref_count_ = new int;
-        *ref_count_ = 1;
-    }
-    else {
-        *ref_count_ += 1;
-    }
-};
+// template <typename T>
+// void SharedPtr<T>::incrementRefCount() {
+//     if (ref_count_ == nullptr) {
+//         ref_count_ = new int;
+//         *ref_count_ = 1;
+//     }
+//     else {
+//         *ref_count_ += 1;
+//     }
+// };
 
-void SharedPtr::decrementAndCleanup() {
-    if (ref_count_ == nullptr) {
-        assert((ptr_ == nullptr) == (ref_count_ == nullptr));
-        return;
-    }
-    else if (*ref_count_ > 1) {
-        *ref_count_ -= 1;
-    }
-    else {
-        delete ptr_;
-        delete ref_count_;
-    }
-}
+// template <typename T>
+// void SharedPtr<T>::decrementAndCleanup() {
+//     if (ref_count_ == nullptr) {
+//         assert((ptr_ == nullptr) == (ref_count_ == nullptr));
+//         return;
+//     }
+//     else if (*ref_count_ > 1) {
+//         *ref_count_ -= 1;
+//     }
+//     else {
+//         delete ptr_;
+//         delete ref_count_;
+//     }
+// }
 
-SharedPtr::~SharedPtr() {
-    decrementAndCleanup();
-}
+// template <typename T>
+// SharedPtr<T>::~SharedPtr() {
+//     decrementAndCleanup();
+// }
 
-SharedPtr::SharedPtr(int value) {
-    ptr_ = new int(value);
-    ref_count_ = new int;
-    *ref_count_ = 1;
-}
+// template <typename T>
+// SharedPtr<T>::SharedPtr(T value) {
+//     ptr_ = new T(value);
+//     ref_count_ = new int;
+//     *ref_count_ = 1;
+// }
 
-// Copy constructor for SharedPtr b = a; (a = other, b is being constructed)
-SharedPtr::SharedPtr(const SharedPtr& other) {
-    ptr_ = other.ptr_;
-    ref_count_ = other.ref_count_;
-    incrementRefCount();
-}
+// // Copy constructor for SharedPtr b = a; (a = other, b is being constructed)
+// template <typename T>
+// SharedPtr<T>::SharedPtr(const SharedPtr& other) {
+//     ptr_ = other.ptr_;
+//     ref_count_ = other.ref_count_;
+//     incrementRefCount();
+// }
 
-// Copy assignment operator (overload  =)
-SharedPtr& SharedPtr::operator=(const SharedPtr& other){
-    if (this == &other) {
-        return *this;
-    }
-    decrementAndCleanup();
+// // Copy assignment operator (overload  =)
+// template <typename T>
+// SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& other){
+//     if (this == &other) {
+//         return *this;
+//     }
+//     decrementAndCleanup();
 
-    ptr_ = other.ptr_;
-    ref_count_ = other.ref_count_;
-    incrementRefCount();
-    return *this;
-}
+//     ptr_ = other.ptr_;
+//     ref_count_ = other.ref_count_;
+//     incrementRefCount();
+//     return *this;
+// }
 
-int SharedPtr::useCount() const {
-    if (ref_count_ == nullptr) {
-        return 0;
-    }
-    else {
-        return *ref_count_;
-    }
-}
+// template <typename T>
+// int SharedPtr<T>::useCount() const {
+//     if (ref_count_ == nullptr) {
+//         return 0;
+//     }
+//     else {
+//         return *ref_count_;
+//     }
+// }
 
-const int* SharedPtr::get() const {
-    return ptr_;
-}
+// template <typename T>
+// const T* SharedPtr<T>::get() const {
+//     return ptr_;
+// }
 
-int& SharedPtr::operator*() {
-    assert(ptr_ != nullptr);
-    return *ptr_;
-}
-const int& SharedPtr::operator*() const {
-    assert(ptr_ != nullptr);
-    return *ptr_; 
-}
+// template <typename T>
+// T& SharedPtr<T>::operator*() {
+//     assert(ptr_ != nullptr);
+//     return *ptr_;
+// }
+
+// template <typename T>
+// const T& SharedPtr<T>::operator*() const {
+//     assert(ptr_ != nullptr);
+//     return *ptr_; 
+// }
