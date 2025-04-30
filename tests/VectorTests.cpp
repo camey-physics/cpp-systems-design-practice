743fa2583816234b0408c3457431e5c3fb5f838d
Chris Amey <callen.amey@gmail.com>
2025-04-30 18:29:17 -0400

    Added unit tests related to the at() function. Tested element access and out of range error throw.

--- tests/VectorTests.cpp
+++ tests/VectorTests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "Vector.h"
+#include <stdexcept>
 
 TEST(VectorTest, DefaultConstructor) {
     Vector<int> v;
@@ -17,4 +18,16 @@ TEST(VectorTest, ConstructorWithNonZeroDefaultInitialization) {
     for (int i = 0; i < 10; ++i) {
         EXPECT_NEAR(v2[i], 3.141592653589793, 1e-14);
     }
+}
+
+TEST(VectorTest, UsingAt) {
+    Vector<double> v2(10, 3.141592653589793);
+    for (int i = 0; i < 10; ++i) {
+        EXPECT_NEAR(v2.at(i), 3.141592653589793, 1e-14);
+    }
+}
+
+TEST(VectorTest, OutOfBoundsErrorWithAt) {
+    Vector<float> v2(10, 3.141592653589793);
+    EXPECT_THROW(v2.at(11), std::out_of_range);
 }
