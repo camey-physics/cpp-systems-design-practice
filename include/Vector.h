eb899fcfbf917ebdbd22ade3a093bfdb9291f792
Chris Amey <callen.amey@gmail.com>
2025-04-29 21:33:42 -0400

    Implemented basic constructor/destructor. Left notes on future functionality to add.

--- include/Vector.h
+++ include/Vector.h
@@ -1,6 +1,13 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+/*
+To Do:
+push_back, pop_back
+operator[] for element access
+Possibly resize or reserve for memory management.
+*/
+
 template <typename T>
 class Vector {
     public:
@@ -11,9 +18,25 @@ class Vector {
 
     private:
         T* ptr_ = nullptr;
-        unsigned long size_ = 0;
+        unsigned long size_;
 };
 
+template <typename T>
+Vector<T>::Vector(unsigned long size, T value) : size_(size) {
+    if (size_ > 0) {
+        ptr_ = new T[size_];
+        for (unsigned long i = 0; i < size_; ++i) {
+            ptr_[i] = value;
+        }
+    }
+    else {
+        ptr_ = nullptr;
+    }
+}
 
+template <typename T>
+Vector<T>::~Vector() {
+    delete[] ptr_;
+}
 
 #endif
