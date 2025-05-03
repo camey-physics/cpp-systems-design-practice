3927789f1c9fe2ca892c05745f0ce3d6be06965c
Chris Amey <callen.amey@gmail.com>
2025-05-03 07:53:04 -0400

    Added several tests related to resize() and = operator to test data management and efficient reallocation.

--- tests/VectorTests.cpp
+++ tests/VectorTests.cpp
@@ -43,12 +43,52 @@ TEST(VectorTest, Reserve) {
     EXPECT_EQ(v.capacity(), 10);
 }
 
-TEST(VectorTest, Resize) {
+TEST(VectorTest, ResizeLarger) {
     Vector<int> v(10,0);
     v.resize(20);
     EXPECT_EQ(v.size(), 20);
 }
 
+TEST(VectorTest, ResizeSmaller) {
+    Vector<int> v(10,0);
+    v.resize(5);
+    EXPECT_EQ(v.size(), 5);
+    EXPECT_EQ(v.capacity(), 10);
+}
+TEST(VectorTest, ResizeReuseIfCapacityLargeEnough) {
+    Vector<int> v(10,0);
+    int* oldPtr = &v[0];
+    v.resize(5);
+    EXPECT_EQ(v.capacity(), 10);
+    EXPECT_EQ(&v[0], oldPtr);
+}
+
+TEST(VectorTest, ResizeInitializesOnlyNewElements) {
+    Vector<int> v(5, 1);
+    v.resize(10);
+    for (int i = 0; i < 5; ++i) {
+        EXPECT_EQ(v[i], 1);
+    }
+    for (int i = 5; i < 10; ++i) {
+        EXPECT_EQ(v[i], 0);
+    }
+}
+
+TEST(VectorTest, AssignmentOperatorReuseIfCapacityLargeEnough) {
+    Vector<float> v(10,1.0), v2(5,3.1415);
+    float* oldPtr = &v[0];
+    v = v2;
+    EXPECT_EQ(v.capacity(), 10);
+    EXPECT_EQ(&v[0], oldPtr);
+}
+
+TEST(VectorTest, SelfAssignment) {
+    Vector<long> v(10,3);
+    v = v;
+    for (int i = 0; i < 10; ++i) {
+        EXPECT_EQ(v[i], 3);
+    }
+}
 TEST(VectorTest, PopBack) {
     Vector<float> v(10);
     v.pop_back();
@@ -62,4 +102,9 @@ TEST(VectorTest, PushBack) {
     EXPECT_EQ(v.size(), 11);
     EXPECT_NEAR(v[10], 3.1415, 1e-7);
     EXPECT_EQ(v.capacity(), 22);
+}
+
+TEST(VectorTest, DataPointerMatchesIndexZero) {
+    Vector<int> v(10, 6);
+    EXPECT_EQ(v.data(), &v[0]);
 }
